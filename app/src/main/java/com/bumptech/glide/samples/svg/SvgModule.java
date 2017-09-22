299ed9c79a40b6d51d4f22aa595b52edf0b84973
Zsago <3427241441@qq.com>
2017-09-22 23:06:04 +0800

    <glide> Fix a couple of errors related to registersComponents refactor.

--- app/src/main/java/com/bumptech/glide/samples/svg/SvgModule.java
+++ app/src/main/java/com/bumptech/glide/samples/svg/SvgModule.java
@@ -2,26 +2,27 @@ package com.bumptech.glide.samples.svg;
 
 import android.content.Context;
 import android.graphics.drawable.PictureDrawable;
-
-import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
-import com.bumptech.glide.module.GlideModule;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
 import com.caverock.androidsvg.SVG;
-
 import java.io.InputStream;
 
 /**
  * Module for the SVG sample app.
  */
-public class SvgModule implements GlideModule {
+@GlideModule
+public class SvgModule extends AppGlideModule {
   @Override
-  public void applyOptions(Context context, GlideBuilder builder) {
-    // Do nothing.
+  public void registerComponents(Context context, Glide glide, Registry registry) {
+    registry.register(SVG.class, PictureDrawable.class, new SvgDrawableTranscoder())
+            .append(InputStream.class, SVG.class, new SvgDecoder());
   }
 
+  // Disable manifest parsing to avoid adding similar modules twice.
   @Override
-  public void registerComponents(Context context, Registry registry) {
-    registry.register(SVG.class, PictureDrawable.class, new SvgDrawableTranscoder())
-        .append(InputStream.class, SVG.class, new SvgDecoder());
+  public boolean isManifestParsingEnabled() {
+    return false;
   }
 }
