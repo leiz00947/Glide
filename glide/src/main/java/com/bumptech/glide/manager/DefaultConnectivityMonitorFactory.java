a0128cb8711a59502578934762e7857de5af7ad9
Zsago <3427241441@qq.com>
2017-09-22 23:22:38 +0800

    <glide> Use PackageManager#checkPermission to check for network state.
    Fixes #2121.

--- glide/src/main/java/com/bumptech/glide/manager/DefaultConnectivityMonitorFactory.java
+++ glide/src/main/java/com/bumptech/glide/manager/DefaultConnectivityMonitorFactory.java
@@ -3,6 +3,7 @@ package com.bumptech.glide.manager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 
 /**
  * A factory class that produces a functional {@link ConnectivityMonitor}
@@ -14,16 +15,15 @@ import android.support.annotation.NonNull;
  * {@link DefaultConnectivityMonitor}实例，否则返回一个{@link NullConnectivityMonitor}实例
  */
 public class DefaultConnectivityMonitorFactory implements ConnectivityMonitorFactory {
+    private static final String NETWORK_PERMISSION = "android.permission.ACCESS_NETWORK_STATE";
+
     @NonNull
     public ConnectivityMonitor build(
             @NonNull Context context,
             @NonNull ConnectivityMonitor.ConnectivityListener listener) {
-        final int res = context.checkCallingOrSelfPermission("android.permission.ACCESS_NETWORK_STATE");
-        final boolean hasPermission = res == PackageManager.PERMISSION_GRANTED;
-        if (hasPermission) {
-            return new DefaultConnectivityMonitor(context, listener);
-        } else {
-            return new NullConnectivityMonitor();
-        }
+        int permissionResult = ContextCompat.checkSelfPermission(context, NETWORK_PERMISSION);
+        boolean hasPermission = permissionResult == PackageManager.PERMISSION_GRANTED;
+        return hasPermission
+                ? new DefaultConnectivityMonitor(context, listener) : new NullConnectivityMonitor();
     }
 }
